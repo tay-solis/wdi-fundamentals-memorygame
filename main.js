3ae41e1e6e92066422447498f3d8a1edd380a0f8
Tay J Solis <tayjsolis@gmail.com>
2018-08-08 15:06:19 -0700

    Completed card shuffling, added event listeners, and card image assets

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 //Possible suites and values for a standard deck of cards
-const suites = ["heart", "diamond", "club", "spade"];
-const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const suites = ["Hearts", "Diamonds", "Clubs", "Spades"];
+const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
 
 
 //For each card in play, creates an array containing the image element.
@@ -13,16 +13,32 @@ let cardsInPlay = [];
 
 //Used to create card objects
 class Card {
-  constructor(suite, value, elm){
+  constructor(suite, value){
   //Suite of Card
     this.suite = suite;
   //Value of Card
     this.value = value;
-  //HTML element
-    this.elm = elm;
+  //Appropriate card image file path
+    this.filePath = "playing-cards-assets/svg-cards/"+ value + "_of_" + suite + ".svg";
   }
 }
 
+//Creates an array of Card objects, representing the game board, based on the number of cards on the board.
+//For each card, there will be a card with the same suite and value.
+function generateCards(){
+  let done = [];
+  for(let i = 0; i < numberOfCardsInPlay / 2; i++){
+    let card = randomCard(i);
+    let check = card.suite + card.value;
+    card = checkIfIncludes(done, check);
+    cardsInPlay.push(card);
+    cardsInPlay.push(card);
+  }
+  shuffle(cardsInPlay);
+  console.log(cardsInPlay);
+}
+
+//Shuffles an array
 function shuffle(arr){
   for(let i = 0; i < arr.length; i++){
     let j = Math.floor(Math.random() * arr.length);
@@ -33,6 +49,7 @@ function shuffle(arr){
   return arr;
 }
 
+//Check if an array already includes check (suite and value of cards). If it does, generates a new card.
 function checkIfIncludes(arr, check, i){
   if(!arr.includes(check)){
     arr.push(check);
@@ -40,32 +57,15 @@ function checkIfIncludes(arr, check, i){
   } else{
     card = randomCard(i)
     check = card.suite + card.value;
-    checkIfIncludes(arr, check, i);
+    checkIfIncludes(arr, check);
   }
 }
 
-
-//Creates an array of Card objects, representing the game board, based on the number of cards on the board.
-//For each card, there will be a card with the same suite and value.
-function generateCards(){
-  let done = [];
-  for(let i = 0; i < numberOfCardsInPlay / 2; i++){
-    let card = randomCard(i);
-    let check = card.suite + card.value;
-    card = checkIfIncludes(done, check, i);
-    cardsInPlay.push(card);
-    cardsInPlay.push(card);
-  }
-  shuffle(cardsInPlay);
-  console.log(cardsInPlay);
-}
-
-
-//Randomly generates a suite and position for each card, with no repeats.
+//Randomly generates a suite and value.
 function randomCard(i){
   let suite = suites[Math.floor(Math.random() * 4)];
   let value = cardValues[Math.floor(Math.random() * 13)];
-  let card = new Card(suite, value, cards[i]);
+  let card = new Card(suite, value);
   return card;
 }
 //Randomly selects half of the positions on the board.
@@ -74,11 +74,17 @@ function randomCard(i){
 //Assigns the card value/suite to the card image elements according to the position on the screen.
 //Sets up event listener for each card.
 function setUpCards(){
+  generateCards();
   for (let i = 0; i < numberOfCardsInPlay; i++){
+      cards[i].setAttribute("alt", cardsInPlay[i].value + " of " + cardsInPlay[i].suite);
       cards[i].addEventListener("click", function(){
-        console.log(this.suite + " " + this.value + " " + this.position);
+        cards[i].setAttribute("src", cardsInPlay[i].filePath);
       });
   }
 }
 
-generateCards();
+function init(){
+  setUpCards();
+}
+
+init();
